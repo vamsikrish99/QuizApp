cee7e5af50ebeba4cda3d11eceafa69c01f81d8a
vamsikrish116 <vsrun2@uis.edu>
2019-08-15 04:36:21 -0700

    Quiz app

---
+++ quizzer.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 19, 2014 at 07:01 PM
+-- Server version: 5.6.16
+-- PHP Version: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `quizzer`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `choices`
+--
+
+CREATE TABLE IF NOT EXISTS `choices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `question_number` int(11) NOT NULL,
+  `is_correct` tinyint(1) NOT NULL DEFAULT '0',
+  `text` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `choices`
+--
+
+INSERT INTO `choices` (`id`, `question_number`, `is_correct`, `text`) VALUES
+(1, 1, 1, 'PHP: Hypertext Preprocessor'),
+(2, 1, 0, 'Private Home Page'),
+(3, 1, 0, 'Personal Home Page'),
+(4, 1, 0, 'Personal Hypertext Preprocessor'),
+(6, 2, 1, 'echo "Hello World";'),
+(7, 2, 0, '"Hello World";'),
+(8, 2, 0, 'Document.Write("Hello World");'),
+(12, 3, 0, 'ASP'),
+(13, 3, 0, 'RUBY'),
+(14, 3, 1, 'PHP');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE IF NOT EXISTS `questions` (
+  `question_number` int(11) NOT NULL,
+  `text` text NOT NULL,
+  PRIMARY KEY (`question_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`question_number`, `text`) VALUES
+(1, 'PHP stands for what?'),
+(2, 'How do you write "Hello World" in PHP\r\n'),
+(3, 'What is the best server side language?');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
